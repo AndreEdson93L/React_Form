78942b7753adf74ddb678db8c6d1251c182c4a71
Andrea Edson Lorenzoni <andrea@gmail.com>
2023-02-19 18:21:28 +0000

    React Input Form. How to handle an Input Form With React.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, createRef } from 'react';
 
 function App() {
+  const [getUserInput, setUserInput] = useState({
+    name: '',
+    nickname: ''
+  });
+
+  const inputRef = createRef();
+
+  const handleUserInputs = event => {
+    const { name, value } = event.target
+    setUserInput({
+      ...getUserInput,
+      [name]: value
+    });
+
+    console.log([name] + ' ' + value);
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('getUserInput.name: ' + getUserInput.name + ', getUserInput.nickname: ' + getUserInput.nickname + ', inputRef: ' + inputRef.current.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input name='name' value={getUserInput.name} onChange={handleUserInputs}/>
+          <input name='nickname' value={getUserInput.nickname} onChange={handleUserInputs}/>
+          <input ref={inputRef} placeholder="lastname"/>
+          <button type='submit'>Register</button>
+        </form>
       </header>
     </div>
   );
